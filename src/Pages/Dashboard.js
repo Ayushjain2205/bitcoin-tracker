2d7300c3107a584dcb0fb6fef6a4061c849b811f
Ayush Jain <ayushjain2205@gmail.com>
2022-02-11 03:37:03 +0530

    Add functionality to get latest bitcoin price from firestore and render with time

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,13 +1,27 @@
-import React from "react";
-import Navigator from "../Components/Navbar";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 import { useUserAuth } from "../context/UserAuthContext";
+import { latestPrice } from "../services/bitcoinService";
 
 function Dashboard() {
   const { user } = useUserAuth();
+  const [price, setPrice] = useState("");
+  const [time, setTime] = useState("");
+
+  useEffect(async () => {
+    const price = await latestPrice();
+    setPrice(price.price);
+
+    // Calculte time
+    var day = moment.unix(price.time);
+    setTime(day.format("h:mm:ss a"));
+  });
+
   return (
     <div>
-      <h1>Dashboard</h1>
       {user && <h1>Welcome {user.email}</h1>}
+      {price && <h1>Current price {price}</h1>}
+      {time && <h1>Last updated @{time} </h1>}
     </div>
   );
 }
---
+++ src/services/bitcoinService.js
@@ -0,0 +1,18 @@
+import { firestore } from "../firebase";
+import { collection, query, orderBy, limit, getDocs } from "firebase/firestore";
+
+export const latestPrice = async () => {
+  // Get all records
+  const priceRef = collection(firestore, "bitcoin");
+  const price = await getDocs(priceRef);
+
+  const all_prices = price.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+  // Sort records by timestamp
+  all_prices.sort(function (x, y) {
+    return x.time - y.time;
+  });
+
+  // return latest record
+  return all_prices.pop();
+};
